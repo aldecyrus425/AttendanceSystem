e8383f48806569129c0a2bbb01ccce19ee9de510
cyrus alde <aldecyrus084@gmail.com>
2026-01-04 12:43:05 +0800

    Initial Commit

---
+++ MyApp.Domain/Entities/GradeLevel.cs
@@ -0,0 +1,45 @@
+﻿using MyApp.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Domain.Entities
+{
+    public class GradeLevel
+    {
+        public int GradeLevelId { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        protected GradeLevel() { } //For ORM
+
+        public GradeLevel(string name, string? description)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+                throw new DomainException("Grade level name is required");
+
+            if (name.Length > 100)
+                throw new DomainException("Grade level name must not exceed 100 characters");
+
+            Name = name;
+            Description = description;
+        }
+
+
+        public void Update(string name, string? description)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+                throw new DomainException("Grade level name is required");
+
+            if (name.Length > 100)
+                throw new DomainException("Grade level name must not exceed 100 characters");
+
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ MyApp.Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Infrastructure.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
+
+
+    }
+}
